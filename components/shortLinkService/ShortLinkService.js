d20a3441fd92760ba146b1adbdf4d9f5adfcd2f3
Pavel <paha_gagarin@mail.ru>
2023-02-21 15:56:34 +0300

    удаление ссылок из рекдакса и копирование ссылок

--- components/shortLinkService/ShortLinkService.js
+++ components/shortLinkService/ShortLinkService.js
@@ -1,24 +1,61 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
+import { addShortLinkActions, deleteShortLinkActions } from "../../pages/store/store";
+import { useDispatch } from "react-redux";
+import { useClipboard } from 'use-clipboard-copy';
 const ShortLinkSrvice = () => {
   const userRegisterState = useSelector((state) => state.userRegistered)
+  const shortLinkState = useSelector((state) => state.shortLinks);
   const [inputValue, setInputValue] = useState('');
   const [inputValidate, setInputvalidate] = useState(false);
   const [buttonDisabled, setButtonDisabled] = useState(true);
+  const [submitForm, setSubmitForm] = useState(false)
+  const addShortLinkDispatch = useDispatch();
+  const deleteShortLinkDispatch = useDispatch();
+  const clipboard = useClipboard();
+
   const inputChangeHandler = (e) => {
     setInputValue(e.target.value)
+  }
+
+  function getShortLink(url) {
+    fetch(`https://api.shrtco.de/v2/shorten?url=${url}`)
+      .then(data => data.json())
+      .then((res) => {
+        return addShortLinkDispatch(addShortLinkActions(res.result))
+      })
+  }
+
+  useEffect(() => {
     if (inputValue !== '') {
       setButtonDisabled(false)
+    } else {
+      setButtonDisabled(true)
     }
+  }, [inputValue, buttonDisabled])
+
+  useEffect(() => {
+    if (submitForm) {
+      getShortLink(inputValue)
+      setSubmitForm(false)
+    }
+  }, [submitForm])
+
+  const deleteShortLinkHandler = (shortLinkId) => {
+    deleteShortLinkDispatch(deleteShortLinkActions(shortLinkId))
   }
+
   const submitFormHandler = (e) => {
     e.preventDefault()
-    if (inputValue !== '' && inputValue.includes('https:/')) {
+    if (inputValue.includes('https:/')) {
       setInputvalidate(false)
+      setSubmitForm(true)
     } else {
       setInputvalidate(true)
+      setSubmitForm(false)
     }
   }
+
   return (
     <section className="short-link-service">
       <div className="container">
@@ -27,7 +64,7 @@ const ShortLinkSrvice = () => {
           onSubmit={submitFormHandler}
         >
           <input
-            className={`form__input ${inputValidate ? "error" : ''}`}
+            className={`form__input`}
             onChange={inputChangeHandler}
             value={inputValue}
             type="text"
@@ -42,15 +79,30 @@ const ShortLinkSrvice = () => {
             <p className="form-inaccessible__text">Need Login</p>
           </div>}
         </form>
-        {/* <ul className="short-link-service__list">
-          <li className="short-link-service__item">
-            <span className="short-link-service__longlink">https:/www.frontendmentor.io</span>
-            <span className="short-link-service__shortlink">https://rel.link/k4iKyk</span>
-            <button className="button short-link-service__button">Copy</button>
-          </li>
-        </ul> */}
+        <ul className="short-link-service__list">
+          {shortLinkState.map((shortLink, index) => {
+            return <li className="short-link-service__item" key={shortLink.code + index}>
+              <span className="short-link-service__longlink">{shortLink.original_link}</span>
+              <input
+                type='text'
+                value={shortLink.full_short_link2}
+                ref={clipboard.target}
+                readOnly
+                className="short-link-service__shortlink"
+              >
+              </input>
+              <button
+                onClick={clipboard.copy}
+                className="button short-link-service__button"
+              >Copy</button>
+              <button onClick={() => deleteShortLinkHandler(shortLink.code)}
+                className="button short-link-service__button"
+              >Delete</button>
+            </li>
+          })}
+        </ul>
       </div>
-    </section>
+    </section >
   )
 }
 
--- pages/store/store.js
+++ pages/store/store.js
@@ -1,23 +1,48 @@
-import { createStore} from 'redux';
+import { createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-const reducer = (state = { userRegistered: false }, action) => {
-  console.log('state',state);
-  console.log('action',action);
+const reducer = (state = { userRegistered: false, shortLinks: [] }, action) => {
   if (action.type === "USER_REGISTERED") {
     return {
-      userRegistered: action.userRegistered
+      ...state,
+      userRegistered: action.userRegistered,
+    }
+  }
+  if (action.type === "ADD_SHORT_LINK") {
+    return {
+      ...state,
+      shortLinks: [...state.shortLinks, action.shortLink]
+    }
+  }
+  if (action.type === "DELETE_SHORT_LINK") {
+    return {
+      ...state,
+      shortLinks: state.shortLinks.filter((shortLink) => shortLink.code !== action.shortLinkId)
     }
   }
   return state;
 }
 
-export const store = createStore(reducer,composeWithDevTools());
+export const store = createStore(reducer, composeWithDevTools());
 
 export const userRegisteredActions = (userRegistered) => {
   return {
     type: 'USER_REGISTERED',
     userRegistered
+  }
+}
+
+export const addShortLinkActions = (shortLink) => {
+  return {
+    type: 'ADD_SHORT_LINK',
+    shortLink
+  }
+}
+
+export const deleteShortLinkActions = (shortLinkId) => {
+  return {
+    type: 'DELETE_SHORT_LINK',
+    shortLinkId
   }
 }
 
